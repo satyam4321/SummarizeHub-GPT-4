6b67c75beb53fe704e27c1ed2ab941ee159cc804
satyam4321 <satyam@an-associates.com>
2023-11-16 00:25:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "./components/Hero.jsx";
+import Demo from "./components/Demo.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="main">
+          <div className="gradient">
+
+          </div>
+        </div>
+        <div className="app">
+          <Hero />
+          <Demo />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
